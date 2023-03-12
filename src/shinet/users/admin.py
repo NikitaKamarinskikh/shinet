393dde61be669cdbe05b68ba09731f327253e6cc
Никита Камаринских <n.kamarinskikh@gmail.com>
2023-03-12 11:47:01 +0700

    add users settings model

--- src/shinet/users/admin.py
+++ src/shinet/users/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users, UserSettings
 
 
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'role', 'email', 'created_at')
+    list_display = ('pk', 'first_name', 'last_name', 'role', 'email', 'created_at')
     list_filter = ('role', )
     search_fields = ('first_name', 'last_name', 'email')
-    list_display_links = ('first_name', 'last_name')
+    list_display_links = ('first_name', 'last_name', 'email')
 
     class Meta:
         model = Users
+
+
+@admin.register(UserSettings)
+class UsersSettingsAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = UserSettings
+
 
--- src/shinet/users/models.py
+++ src/shinet/users/models.py
@@ -9,6 +9,8 @@ class Users(models.Model):
     password = models.CharField(verbose_name='Пароль', max_length=255)
     sex = models.CharField(verbose_name='Пол', max_length=30)
     created_at = models.DateTimeField(verbose_name='Дата регистрации', auto_now_add=True)
+    settings = models.OneToOneField('UserSettings', on_delete=models.PROTECT,
+                                    null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -16,4 +18,12 @@ class Users(models.Model):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class UserSettings(models.Model):
+    color_theme = models.CharField(verbose_name='Цветовая тема', max_length=255)
+
+    class Meta:
+        verbose_name = 'Настройки пользователя'
+        verbose_name_plural = 'Настройки пользователей'
 
