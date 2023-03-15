68750e81e1d0f72b276c9bb0be4f4ee61b3e8b07
Никита Камаринских <n.kamarinskikh@gmail.com>
2023-03-15 23:26:15 +0700

    include rest_framework and swagger

--- src/shinet/shinet/settings.py
+++ src/shinet/shinet/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'drf_yasg',
+
     'users.apps.UsersConfig',
     'services.apps.ServicesConfig',
     'tokens.apps.TokensConfig',
