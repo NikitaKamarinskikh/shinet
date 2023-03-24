cd98f8b80c8a3688d6b8157a8a2945c43ae7ded4
Никита Камаринских <n.kamarinskikh@gmail.com>
2023-03-24 15:42:34 +0700

    add location parameter for masters registration

--- src/shinet/users/masters/serializers.py
+++ src/shinet/users/masters/serializers.py
@@ -15,10 +15,12 @@ class MasterCreationSerializer(serializers.Serializer):
     phone_numbers_lit = serializers.ListField(
         child=serializers.CharField(), required=False
     )
+    location = serializers.CharField(max_length=255)
 
     def create(self, validated_data):
         validated_data['password'] = make_sha256_hash(validated_data['password'])
         del validated_data['specializations_ids_list']
         del validated_data['phone_numbers_lit']
+        del validated_data['location']
         return Users.objects.create(**validated_data)
 
