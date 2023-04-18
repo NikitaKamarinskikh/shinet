6aacd83b35388a5a0d8cba154775194df9df0b80
Никита Камаринских <n.kamarinskikh@gmail.com>
2023-04-18 14:48:17 +0700

    update tests

--- src/shinet/users/tests.py
+++ src/shinet/users/tests.py
@@ -8,34 +8,33 @@ class ClientRegistrationTest(APITestCase):
 
     def test_registration_1(self):
         """
-        asd
+        Test client registration
         """
-        url = '/api/v1/users/clients/registration/'
+        url = '/api/v1/registration/client/'
         data = {
             'email': 'user@example.com',
             'password': 'string',
             'first_name': 'string',
             'last_name': 'string',
             'sex': 'string',
-            'role': 'string'
         }
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Users.objects.count(), 1)
         self.assertEqual(Users.objects.get().email, 'user@example.com')
 
     def test_registration_2(self):
-        url = '/api/v1/users/clients/registration/'
+        url = '/api/v1/registration/client/'
         data = {
             'email': 'user@example.com',
             'password': 'string',
             'first_name': 'string',
         }
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     def test_registration_3(self):
-        url = '/api/v1/users/clients/registration/'
+        url = '/api/v1/registration/client/'
         data = {
             'email': 'rrrrr',
             'password': 'string',
@@ -45,19 +44,19 @@ class ClientRegistrationTest(APITestCase):
             'role': 'string'
         }
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-    def test_auth_1(self):
-        url = '/api/v1/users/auth/'
-        data = {'email': 'aaaaa', 'password': 'aaaaaa'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_auth_2(self):
-        url = '/api/v1/users/auth/'
-        data = {'email': 'user@example.com'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    # def test_auth_1(self):
+    #     url = '/api/v1/users/auth/'
+    #     data = {'email': 'aaaaa', 'password': 'aaaaaa'}
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+    #
+    # def test_auth_2(self):
+    #     url = '/api/v1/users/auth/'
+    #     data = {'email': 'user@example.com'}
+    #     response = self.client.post(url, data, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 
