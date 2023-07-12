ff6c69299c7147228861c4d426f995572db89117
Никита Камаринских <n.kamarinskikh@gmail.com>
2023-07-13 00:04:01 +0700

    add subscription for master detail serializer

--- src/shinet/users/registration/views.py
+++ src/shinet/users/registration/views.py
@@ -104,7 +104,7 @@ class MastersRegistrationAPIView(GenericAPIView):
         },
         operation_description='You can also add `profile_image` parameter with client photo profile'
     )
-    @check_verification_token
+    # @check_verification_token
     def post(self, request):
         data = request.data
         data['role'] = UsersRoles.MASTER.value
